46ffcce14253b10d5f2916dfd98423e0831eb0f6
MICKY <a9545096@gmail.com>
2026-06-30 12:10:02 +0800

    PsychologistVO

---
+++ src/main/java/com/freemind/login/psychologist/moder/PsychologistRepository.java
@@ -0,0 +1,7 @@
+package com.freemind.login.psychologist.moder;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface  PsychologistRepository extends JpaRepository<Psychologist, Integer> {
+
+
+}
