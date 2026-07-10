73f2fed4511553b8c8001842cf5773f781764596
MICKY <a9545096@gmail.com>
2026-07-11 06:01:57 +0800

    PsychologistExpertise

--- src/main/java/com/freemind/login/psychologist/entity/PsychologistExpertise.java
+++ src/main/java/com/freemind/login/psychologist/entity/PsychologistExpertise.java
@@ -59,7 +59,7 @@ public class PsychologistExpertise {
 	@Embeddable
 	public static class CompositeExpertiseDetail implements Serializable{
 		
-		@Column(name  = "psych_id")
+		@Column(name = "psych_id")
 		private Integer psychId;
 		
 		@Column(name = "expertise_id")
@@ -83,8 +83,8 @@ public class PsychologistExpertise {
 			return psychId;
 		}
 
-		public void setPsychId(Integer psychID) {
-			this.psychId = psychID;
+		public void setPsychId(Integer psychId) {
+			this.psychId = psychId;
 		}
 
 		public Integer getExpertiseId() {
