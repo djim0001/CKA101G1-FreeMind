bb38aab0acaa1a0150ee82affedd056375f9b2be
LIU <joshliu7106@gmail.com>
2026-07-18 17:02:52 +0800

    add member and admin-css、js

--- src/main/java/com/freemind/login/member/controller/MemberDashboardController.java
+++ src/main/java/com/freemind/login/member/controller/MemberDashboardController.java
@@ -3,25 +3,37 @@ package com.freemind.login.member.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.freemind.article.entity.Article;
 import com.freemind.article.service.ArticleInteractionService;
+import com.freemind.login.member.model.Member;
+import com.freemind.login.member.model.MemberService;
 import com.freemind.login.security.membersecurity.MemberUserDetails;
 
 
-@Controller      
+@Controller
 @RequestMapping("/member/dashboard")
-public class MemberDashboardController {  
-	
+public class MemberDashboardController {
+
 	@Autowired
 	private ArticleInteractionService articleInteractionService;
-	
+
+	@Autowired
+	private MemberService memberService;
+
+	@ModelAttribute("member")
+	public Member currentMember(Authentication authentication) {
+		return memberService.findByAccount(authentication.getName());
+	}
+
 	@GetMapping("/myAppointment")
 	public String myAppointment() {
 	    return "front-end/member/memberpage/myAppointment";
